fdc8343a394636dfb84bce294cf9d5b22761c72c
Ali Khakpouri <ali.khakpouri@gmail.com>
2026-03-12 12:00:17 -0400

    updated the tax test cases. finished it

--- api/internal/services/tax/tax_service_test.go
+++ api/internal/services/tax/tax_service_test.go
@@ -13,6 +13,9 @@ func TestGetStates(t *testing.T) {
 	states := svc.GetStates()
 	fmt.Printf("total number of states is %d \n", len(states))
 	assert.Equal(t, 51, len(states), "They should be equal")
+	for i := 1; i < len(states); i++ {
+		assert.GreaterOrEqual(t, states[i], states[i-1])
+	}
 }
 
 func TestCalculate(t *testing.T) {
@@ -27,6 +30,22 @@ func TestInvalidStateCalculate(t *testing.T) {
 	svc := NewTaxService()
 	amount, state := 100.00, "BC"
 	tax, err := svc.Calculate(amount, state)
-	assert.NoError(t, err)
+	assert.Error(t, err)
 	assert.Nil(t, tax)
+}
+
+func TestZeroTaxState(t *testing.T) {
+	svc := NewTaxService()
+	amount, state := 100.00, "DE"
+	tax, err := svc.Calculate(amount, state)
+	assert.NoError(t, err)
+	assert.Equal(t, 0.0, *tax, "They should be equal")
+}
+
+func TestZeroAmount(t *testing.T) {
+	svc := NewTaxService()
+	amount, state := 0.00, "CA"
+	tax, err := svc.Calculate(amount, state)
+	assert.NoError(t, err)
+	assert.Equal(t, 0.0, *tax, "They should be equal")
 }
